ebeae611194c92b5c34abca85696cbc0ad02ab1d
Hale <Hale@6ae6004f-9e4d-49be-878d-5da08febfbad>
2014-11-26 10:40:45 +0000

    git-svn-id: https://viruscollector/svn/Spacecraft@5 6ae6004f-9e4d-49be-878d-5da08febfbad

---
+++ src/Spacecraftcore/MainGame.java
@@ -0,0 +1,13 @@
+package Spacecraftcore
+public class MainGame {
+	/**
+	 * This class contains main method 
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

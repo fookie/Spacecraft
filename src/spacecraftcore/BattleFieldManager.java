be3c1750d72769886f7c736d5a4e921de5240c1f
Hale <Hale@6ae6004f-9e4d-49be-878d-5da08febfbad>
2014-11-27 13:58:37 +0000

    git-svn-id: https://viruscollector/svn/Spacecraft@20 6ae6004f-9e4d-49be-878d-5da08febfbad

--- src/spacecraftcore/BattleFieldManager.java
+++ src/spacecraftcore/BattleFieldManager.java
@@ -4,16 +4,48 @@ import java.util.LinkedList;
 import java.util.List;
 
 import spacecraftelements.*;
-
+/**
+ * BattleFieldManager负责更新战场以及地图，并把信息传递给DisplayManager
+ * @author Hale
+ *
+ */
 public class BattleFieldManager {
-	List<Bullet> BulletList = new LinkedList<Bullet>();
-	SpaceShip Ship=null;
+	private List<Bullet> BulletList = new LinkedList<Bullet>();
+	private List<Enemy> EnemyList = new LinkedList<Enemy>();
+	private SpaceShip Ship = null;
+	
+	/**
+	 * 添加子弹
+	 * 
+	 * @param Bullet
+	 * @return 是否成功添加
+	 */
 	public boolean add(Bullet b) {
 		return BulletList.add(b);
 	}
+
+	/**
+	 * 添加SpaceShip，只能添加一次
+	 * 
+	 * @param SpaceShip
+	 * @return 是否成功添加
+	 */
 	public boolean add(SpaceShip s) {
-		Ship=s;
-		return true;
+		if (Ship == null) {
+			Ship = s;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	/**
+	 * 添加敌人
+	 * 
+	 * @param Bullet
+	 * @return 是否成功添加
+	 */
+	public boolean add(Enemy e) {
+		return EnemyList.add(e);
 	}
 	
 }

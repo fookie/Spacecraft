1adca1cc08dad8e0356f6ce4d5378531b34b3c73
Hale <Hale@6ae6004f-9e4d-49be-878d-5da08febfbad>
2014-11-28 04:02:29 +0000

    git-svn-id: https://viruscollector/svn/Spacecraft@25 6ae6004f-9e4d-49be-878d-5da08febfbad

---
+++ src/spacecraftTest/BattleFieldTest.java
@@ -0,0 +1,17 @@
+package spacecraftTest;
+
+import spacecraftcore.BattleFieldManager;
+
+/**
+ * 测试BattleFieldManager相关功能
+ * @author Hale
+ *
+ */
+public class BattleFieldTest {
+	public static void main(String[] args)
+	{
+		BattleFieldManager bfm=new BattleFieldManager();
+		bfm.loadmap("scmaps//ceshiditu.cmp",21, 13);
+	}
+
+}
--- src/spacecraftcore/BattleFieldManager.java
+++ src/spacecraftcore/BattleFieldManager.java
@@ -1,11 +1,18 @@
 package spacecraftcore;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
 import spacecraftelements.*;
+
 /**
  * BattleFieldManager负责更新战场以及地图，并把信息传递给DisplayManager
+ * 
  * @author Hale
  *
  */
@@ -13,11 +20,33 @@ public class BattleFieldManager {
 	private List<Bullet> BulletList = new LinkedList<Bullet>();
 	private List<Enemy> EnemyList = new LinkedList<Enemy>();
 	private SpaceShip Ship = null;
-	public boolean loadmap(String mapaddress,int x,int y)
-	{
+	private int map[][];
+	private DataInputStream mapin;
+	public boolean loadmap(String mapaddress, int x, int y) {
+		try {
+			mapin = new DataInputStream(
+					new BufferedInputStream(new FileInputStream(mapaddress)));
+		} catch (FileNotFoundException e) {
+			System.out.println(mapaddress + " 不存在，故无法加载地图");
+			e.printStackTrace();// 会出现红字
+			return false;
+		}
+		map = new int[x][y];
+		for (int column = 0; column < y; column++) {
+			for (int row = 0; row < x; row++) {
+				try {
+					map[row][column]=mapin.readInt();
+				} catch (IOException e) {
+					System.out.println("在读取地图中 row: "+row+" colum: "+column+" 无法处理或读取该数据，故无法加载地图");
+					e.printStackTrace();
+					return false;
+				}	
+			}
+		}
 		return true;
-		
+
 	}
+
 	/**
 	 * 添加子弹
 	 * 
@@ -27,6 +56,7 @@ public class BattleFieldManager {
 	public boolean add(Bullet b) {
 		return BulletList.add(b);
 	}
+
 	/**
 	 * 添加SpaceShip，只能添加一次
 	 * 
@@ -41,6 +71,7 @@ public class BattleFieldManager {
 			return false;
 		}
 	}
+
 	/**
 	 * 添加敌人
 	 * 
@@ -50,5 +81,5 @@ public class BattleFieldManager {
 	public boolean add(Enemy e) {
 		return EnemyList.add(e);
 	}
-	
+
 }

bd46fc0e69274e83b23c82aa3e0a67ad65ce9536
Hale <Hale@6ae6004f-9e4d-49be-878d-5da08febfbad>
2014-11-29 12:15:17 +0000

    git-svn-id: https://viruscollector/svn/Spacecraft@49 6ae6004f-9e4d-49be-878d-5da08febfbad

--- src/spacecraftelements/Bullets/BasicBullet.java
+++ src/spacecraftelements/Bullets/BasicBullet.java
@@ -1,11 +1,28 @@
 package spacecraftelements.Bullets;
 
+import spacecraftcore.MainGame;
+
 public class BasicBullet extends Bullet{
-	public BasicBullet(){
-		
+	public BasicBullet(Double x,Double y){
+		//设定该子弹属性
+		super.x=x;
+		super.y=y;
+		super.vx=(double) 10;
+		super.vy=(double) 8;
+		super.damage=15.0;
+		super.ImageID="Image//bullet//basicbullet.png";
+		//角度暂时不设定
 	}
 
+	@Override
 	public boolean update() {
+		x=x+vx;
+		y=y+vy;
+		if(x>(MainGame.bm.mapx)/2||y>(MainGame.bm.mapy)/2||x<-(MainGame.bm.mapx)/2||y<-(MainGame.bm.mapy)/2)
+		{//超出边界
 		return false;
+		}
+		return true;
 	}
+
 }

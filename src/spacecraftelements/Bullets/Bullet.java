0f6a7ea32809b13491f02f1a3c06cde03d9e579a
Paulay <Paulay@6ae6004f-9e4d-49be-878d-5da08febfbad>
2014-11-29 10:18:13 +0000

    git-svn-id: https://viruscollector/svn/Spacecraft@45 6ae6004f-9e4d-49be-878d-5da08febfbad

--- src/spacecraftelements/Bullets/Bullet.java
+++ src/spacecraftelements/Bullets/Bullet.java
@@ -8,30 +8,30 @@ public abstract class Bullet {
 	/**
 	 * 		子弹贴图路径
 	 */
-	String ImageID;
+	public String ImageID;
 	/**
 	 * 		子弹速度
 	 */
-	Double vx,vy;
+	public Double vx,vy;
 	/**
 	 * 		子弹加速度（可选）
 	 */
-	Double ax, ay;
+	public Double ax, ay;
 	/**
 	 * 		子弹初始生成位置
 	 */
-	Double x, y;
+	public Double x, y;
 	/**
 	 *		子弹朝向角度 
 	 */
-	Double angle;
+	public Double angle;
 	/**
 	 * 		子弹伤害值
 	 */
-	Double damage;
+	public Double damage;
 	/**
 	 * 		子弹所属（跟踪类武器需要）
 	 * 		用于子弹判断敌我
 	 */
-	int faction;
+	public int faction;
 }

e1e0a6365ed767511b2ba84961b591f801b20fae
Hale <Hale@6ae6004f-9e4d-49be-878d-5da08febfbad>
2014-11-29 13:00:53 +0000

    git-svn-id: https://viruscollector/svn/Spacecraft@57 6ae6004f-9e4d-49be-878d-5da08febfbad

--- src/spacecraftelements/Bullets/BasicBullet.java
+++ src/spacecraftelements/Bullets/BasicBullet.java
@@ -2,29 +2,27 @@ package spacecraftelements.Bullets;
 
 import spacecraftcore.MainGame;
 
-/**
- * 基础子弹，暂时用于测试
- */
-public class BasicBullet extends Bullet {
-	public BasicBullet(int x, int y) {
-		// 设定该子弹属性
-		super.x = x;
-		super.y = y;
-		super.vx = 10;
-		super.vy = 8;
-		super.damage = 15;
-		super.ImageID = "Images//bullet//basicbullet.png";
+public class BasicBullet extends Bullet{
+	public BasicBullet(int x,int y,int vx,int vy){
+		//设定该子弹属性
+		super.x=x;
+		super.y=y;
+		super.vx=vx;
+		super.vy=vy;
+		super.damage=15;
 		super.volume = 10;
-		// 角度暂时不设定
+		super.ImageID="Images//bullet//basicbullet.png";
+		//角度暂时不设定
 	}
 
 	@Override
 	public boolean update() {
-		x = x + vx;
-		y = y + vy;
-		if (x > (MainGame.bm.mapx) / 2 || y > (MainGame.bm.mapy) / 2
-				|| x < -(MainGame.bm.mapx) / 2 || y < -(MainGame.bm.mapy) / 2) {// 超出边界
-			return false;
+		System.out.print(this.toString()+"before: x");
+		x=x+vx;
+		y=y+vy;
+		if(x>(MainGame.bm.mapx)/2||y>(MainGame.bm.mapy)/2||x<-(MainGame.bm.mapx)/2||y<-(MainGame.bm.mapy)/2)
+		{//超出边界
+		return false;
 		}
 		return true;
 	}

d451533fd4f59f6df01dc6315615b81a4be4ccf6
Paulay <Paulay@6ae6004f-9e4d-49be-878d-5da08febfbad>
2014-11-29 12:59:35 +0000

    git-svn-id: https://viruscollector/svn/Spacecraft@56 6ae6004f-9e4d-49be-878d-5da08febfbad

--- src/spacecraftcore/BattleFieldManager.java
+++ src/spacecraftcore/BattleFieldManager.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import spacecraftelements.*;
 import spacecraftelements.Bullets.Bullet;
+import spacecraftelements.Enemy.Enemy;
 
 /**
  * BattleFieldManager负责更新战场以及地图，并把信息传递给DisplayManager

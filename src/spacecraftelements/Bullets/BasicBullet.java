a323da2fd5dd672acb3ff09f34718fd1cc424a10
Paulay <Paulay@6ae6004f-9e4d-49be-878d-5da08febfbad>
2014-11-29 12:54:30 +0000

    所有Double变量改为int
    git-svn-id: https://viruscollector/svn/Spacecraft@54 6ae6004f-9e4d-49be-878d-5da08febfbad

--- src/spacecraftelements/Bullets/BasicBullet.java
+++ src/spacecraftelements/Bullets/BasicBullet.java
@@ -1,29 +1,30 @@
 package spacecraftelements.Bullets;
 
 import spacecraftcore.MainGame;
+
 /**
- *		基础子弹，暂时用于测试 
+ * 基础子弹，暂时用于测试
  */
-public class BasicBullet extends Bullet{
-	public BasicBullet(Double x,Double y){
-		//设定该子弹属性
-		super.x=x;
-		super.y=y;
-		super.vx=(double) 10;
-		super.vy=(double) 8;
-		super.damage=15.0;
-		super.ImageID="Images//bullet//basicbullet.png";
-		super.volume = 10.0;
-		//角度暂时不设定
+public class BasicBullet extends Bullet {
+	public BasicBullet(int x, int y) {
+		// 设定该子弹属性
+		super.x = x;
+		super.y = y;
+		super.vx = 10;
+		super.vy = 8;
+		super.damage = 15;
+		super.ImageID = "Images//bullet//basicbullet.png";
+		super.volume = 10;
+		// 角度暂时不设定
 	}
 
 	@Override
 	public boolean update() {
-		x=x+vx;
-		y=y+vy;
-		if(x>(MainGame.bm.mapx)/2||y>(MainGame.bm.mapy)/2||x<-(MainGame.bm.mapx)/2||y<-(MainGame.bm.mapy)/2)
-		{//超出边界
-		return false;
+		x = x + vx;
+		y = y + vy;
+		if (x > (MainGame.bm.mapx) / 2 || y > (MainGame.bm.mapy) / 2
+				|| x < -(MainGame.bm.mapx) / 2 || y < -(MainGame.bm.mapy) / 2) {// 超出边界
+			return false;
 		}
 		return true;
 	}
--- src/spacecraftelements/Bullets/Bullet.java
+++ src/spacecraftelements/Bullets/Bullet.java
@@ -15,23 +15,23 @@ public abstract class Bullet implements Updatable{
 	/**
 	 * 		子弹速度
 	 */
-	public Double vx,vy;
+	public int vx,vy;
 	/**
 	 * 		子弹加速度（可选）
 	 */
-	public Double ax, ay;
+	public int ax, ay;
 	/**
 	 * 		子弹初始生成位置
 	 */
-	public Double x, y;
+	public int x, y;
 	/**
 	 *		子弹朝向角度 	
 	 */
-	public Double angle;
+	public int angle;
 	/**
 	 * 		子弹伤害值
 	 */
-	public Double damage;
+	public int damage;
 	/**
 	 * 		子弹所属（跟踪类武器需要）
 	 * 		用于子弹判断敌我
@@ -40,6 +40,6 @@ public abstract class Bullet implements Updatable{
 	/**
 	 * 		子弹体积
 	 */
-	public Double volume;
+	public int volume;
 	public abstract boolean update();
 }

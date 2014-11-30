4607856b16db34d0e41ece0c958a301522172de3
Paulay <Paulay@6ae6004f-9e4d-49be-878d-5da08febfbad>
2014-11-30 07:23:30 +0000

    git-svn-id: https://viruscollector/svn/Spacecraft@87 6ae6004f-9e4d-49be-878d-5da08febfbad

--- src/spacecraftelements/SpaceShip/SpaceShip.java
+++ src/spacecraftelements/SpaceShip/SpaceShip.java
@@ -11,15 +11,15 @@ public abstract class SpaceShip implements Updatable{
 	/**
 	 * 游戏内建坐标
 	 */
-	Double x,y;
+	public int x,y;
 	/**
 	 * 飞船加速度，先不考虑
 	 */
-	Double ax,ay;
+	public int ax,ay;
 	/**
 	 * 速度(趋势)，每次更新时用
 	 */
-	Double vx,vy;
+	public int vx,vy;
 	/**
 	 * 贴图地址，以后要是有必要可以改成ImageID[];
 	 */

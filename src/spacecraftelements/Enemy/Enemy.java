46863c1584b2e55209e5e69bd8d58ec33ae16625
Paulay <Paulay@6ae6004f-9e4d-49be-878d-5da08febfbad>
2014-12-01 13:03:07 +0000

    完成Slime和Enemy
    git-svn-id: https://viruscollector/svn/Spacecraft@116 6ae6004f-9e4d-49be-878d-5da08febfbad

--- src/spacecraftelements/Enemy/Enemy.java
+++ src/spacecraftelements/Enemy/Enemy.java
@@ -7,7 +7,25 @@ public class Enemy implements Updatable{
 	 *  敌人生成位置
 	 */
 	public int x, y;
-	
+	/**
+	 * 	敌人攻击力
+	 */
+	public int damage;
+	/**
+	 *  敌人移动速度
+	 */
+	public int v;
+	/**
+	 * 	敌人血量
+	 */
+	public int health;
+	/**
+	 * 	敌人体积
+	 */
+	public int volume;
+	/**
+	 * 	敌人自更新
+	 */
 	public boolean update() {
 		return false;
 	}
--- src/spacecraftelements/Enemy/Slime.java
+++ src/spacecraftelements/Enemy/Slime.java
@@ -1,7 +1,27 @@
 package spacecraftelements.Enemy;
 
-public class Slime extends Enemy{
-	public Slime(int x, int y){
-		
+import spacecraftcore.MainGame;
+
+public class Slime extends Enemy {
+	public Slime(int x, int y) {
+		super.x = x;
+		super.y = y;
+		super.damage = 1;
+		super.health = 50;
+		super.v = 10;
+		super.volume = 20;
+	}
+
+	public boolean update() {
+		int x1, y1, vx, vy;
+		double angle;
+		x1 = MainGame.bm.getShip().x - x;
+		y1 = MainGame.bm.getShip().y - y;
+		angle = MainGame.bm.getangle(x1, y1);
+		vx = (int) (Math.cos(angle) * v);
+		vy = (int) (Math.sin(angle) * v);
+		x = x + vx;
+		y = y + vy;
+		return true;
 	}
 }

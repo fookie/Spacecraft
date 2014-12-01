ef652a9ae0fe4469458ad46f4da4f17c22a8bece
EveLIn <EveLIn@6ae6004f-9e4d-49be-878d-5da08febfbad>
2014-12-01 07:33:34 +0000

    git-svn-id: https://viruscollector/svn/Spacecraft@108 6ae6004f-9e4d-49be-878d-5da08febfbad

---
+++ src/spacecraftcore/ML.java
@@ -0,0 +1,19 @@
+package spacecraftcore;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class ML implements MouseMotionListener{
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		
+	}
+
+}

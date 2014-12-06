81628ac54556c3a8e8802d20ab3f4bedc079237a
Paulay <Paulay@6ae6004f-9e4d-49be-878d-5da08febfbad>
2014-12-06 04:30:10 +0000

    优化了追踪算法，现在敌人可以稳定并精确地追踪飞船了
    git-svn-id: https://viruscollector/svn/Spacecraft@124 6ae6004f-9e4d-49be-878d-5da08febfbad

--- src/spacecraftelements/Enemy/Slime.java
+++ src/spacecraftelements/Enemy/Slime.java
@@ -8,20 +8,25 @@ public class Slime extends Enemy {
 		super.y = y;
 		super.damage = 1;
 		super.health = 50;
-		super.v = 10;
+		super.v = 4;
 		super.volume = 20;
 		super.imageID = "Images//ornament//basicentity.png";
-		super.imagesize=20;
+		super.imagesize = 20;
 	}
 
 	public boolean update() {
+		if (x == MainGame.bm.getShip().x && y == MainGame.bm.getShip().y) {
+			return false;
+		}
 		int x1, y1;
-		double angle;
+		double ratiox, ratioy, third;
 		x1 = MainGame.bm.getShip().x - x;
 		y1 = MainGame.bm.getShip().y - y;
-		angle = MainGame.bm.getangle(x1, y1);
-		vx = (int) (Math.cos(angle) * v);
-		vy = (int) (Math.sin(angle) * v);
+		third = Math.sqrt((double) x1 * x1 + y1 * y1);
+		ratiox = ((double) x1) / third;
+		ratioy = ((double) y1) / third;
+		vx = (int) (((double) v) * ratiox);
+		vy = (int) (((double) v) * ratioy);
 		x = x + vx;
 		y = y + vy;
 		return true;

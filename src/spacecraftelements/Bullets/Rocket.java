efff19a09a3938f5f96442f6230effa0abd437b5
Paulay <Paulay@6ae6004f-9e4d-49be-878d-5da08febfbad>
2015-03-23 23:57:21 +0000

    优化了飞弹
    git-svn-id: https://viruscollector/svn/Spacecraft@218 6ae6004f-9e4d-49be-878d-5da08febfbad

--- src/spacecraftelements/Bullets/Rocket.java
+++ src/spacecraftelements/Bullets/Rocket.java
@@ -13,7 +13,7 @@ public class Rocket extends Bullet {
 		this.fay = fay;
 		super.damage = 50;
 		super.volume = 20;
-		super.ImageID = "Images//bullet//basicbullet2.png";
+		super.ImageID = "Images//bullet//Rocket.png";
 		super.ID = 2;
 		super.angle = 0;
 		super.imagesizex = 10;
@@ -21,33 +21,19 @@ public class Rocket extends Bullet {
 		super.faction = f;
 	}
 	public boolean update() {
-//		if(Math.abs(ax) < Math.abs(fax)){
-//			if(fax <0)
-//			{
-//				ax--;
-//			}else if (fax>0){
-//				ax++;
-//			}
-//		}
-//		if(Math.abs(ay) < Math.abs(fay)){
-//			if(fay <0)
-//			{
-//				ay--;
-//			}else if (fay>0){
-//				ay++;
-//			}
-//		}
 		x += vx;
 		y += vy;
-		vx += ax;
 		if((vx + ax) * vx <= 0){
 			vx = 0;
 			ax = fax;
+			ImageID = "Images//bullet//Rocketac.png";
 		}
 		if((vy + ay)*vy <= 0){
 			vy = 0;
 			ay = fay;
+			ImageID = "Images//bullet//Rocketac.png";
 		}
+		vx += ax;
 		vy += ay;
 		return true;
 	}

d7d77c13e3979416d4b34fdcd98b70d53f983bb5
Paulay <Paulay@6ae6004f-9e4d-49be-878d-5da08febfbad>
2015-04-10 13:41:55 +0000

    1.减小了武器栏 2.加入了JoAL库 3.不要动Soundmanager先 4.修改了超出血量后的血量显示
    git-svn-id: https://viruscollector/svn/Spacecraft@243 6ae6004f-9e4d-49be-878d-5da08febfbad

--- src/displayConsole/WeaponSlot.java
+++ src/displayConsole/WeaponSlot.java
@@ -12,16 +12,16 @@ public class WeaponSlot {
 	public static boolean displayweapon(SpaceShip s){
 		
 		if(s.w1.weaponID == 0){
-			MainGame.test.repainter.add_nooffset_element("Images//InGameHUD//Weaponslot//hightech.png",MainGame.test.repainter.onscreenx(0), MainGame.test.repainter.onscreeny(-MainGame.test.windowsizey+360), 0, 2);
+			MainGame.test.repainter.add_nooffset_element("Images//InGameHUD//Weaponslot//hightech.png",MainGame.test.repainter.onscreenx(0), MainGame.test.repainter.onscreeny(-MainGame.test.windowsizey+270), 0, 2);
 			//MainGame.test.repainter.add_nooffset_element(s.w1.ImageLoc, MainGame.test.repainter.onscreenx(0), MainGame.test.repainter.onscreeny(-110), 0, 2);
 		}else if(s.w1.weaponID == 3){
-			MainGame.test.repainter.add_nooffset_element("Images//InGameHUD//Weaponslot//rocket.png",MainGame.test.repainter.onscreenx(0), MainGame.test.repainter.onscreeny(-MainGame.test.windowsizey+360), 0, 2);
+			MainGame.test.repainter.add_nooffset_element("Images//InGameHUD//Weaponslot//rocket.png",MainGame.test.repainter.onscreenx(0), MainGame.test.repainter.onscreeny(-MainGame.test.windowsizey+270), 0, 2);
 			//MainGame.test.repainter.add_nooffset_element(emptyslot, MainGame.test.repainter.onscreenx(0), MainGame.test.repainter.onscreeny(-110), 0, 2);
 		}else if(s.w1.weaponID == 2){
-			MainGame.test.repainter.add_nooffset_element("Images//InGameHUD//Weaponslot//burst.png",MainGame.test.repainter.onscreenx(0), MainGame.test.repainter.onscreeny(-MainGame.test.windowsizey+360), 0, 2);
+			MainGame.test.repainter.add_nooffset_element("Images//InGameHUD//Weaponslot//burst.png",MainGame.test.repainter.onscreenx(0), MainGame.test.repainter.onscreeny(-MainGame.test.windowsizey+270), 0, 2);
 			//MainGame.test.repainter.add_nooffset_element(emptyslot, MainGame.test.repainter.onscreenx(100), MainGame.test.repainter.onscreeny(-110), 0, 2);
 		}else if(s.w1.weaponID == 1){
-			MainGame.test.repainter.add_nooffset_element("Images//InGameHUD//Weaponslot//mass.png",MainGame.test.repainter.onscreenx(0), MainGame.test.repainter.onscreeny(-MainGame.test.windowsizey+360), 0, 2);
+			MainGame.test.repainter.add_nooffset_element("Images//InGameHUD//Weaponslot//mass.png",MainGame.test.repainter.onscreenx(0), MainGame.test.repainter.onscreeny(-MainGame.test.windowsizey+270), 0, 2);
 			//MainGame.test.repainter.add_nooffset_element(s.w2.ImageLoc, MainGame.test.repainter.onscreenx(100), MainGame.test.repainter.onscreeny(-110), 0, 2);
 		}
 		return false;
---
+++ src/spacecraftcore/SoundManager.java
@@ -0,0 +1,14 @@
+package spacecraftcore;
+
+import java.io.IOException;
+
+import com.jogamp.openal.UnsupportedAudioFileException;
+
+public class SoundManager {
+	public SoundManager() {
+	}
+	
+	public void init() throws UnsupportedAudioFileException, IOException{
+		
+	}
+}
